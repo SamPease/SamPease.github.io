146dc02bea855d131033cc6ebbd7599f7c73a734
Samantha Pease <sam@walking-stick.com>
2026-06-21 13:13:54 -0400

    redirects old links now

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,12 @@ import rehypeKatex from "rehype-katex";
 export default defineConfig({
   site: "https://sampease.github.io",
   output: "static",
+  redirects: {
+    "/barselo": "/project-writeups/barselo/",
+    "/instagram-network": "/project-writeups/instagram-network/",
+    "/retraining-unlearning": "/project-writeups/retraining-unlearning/",
+    "/trans-advice-agent": "/project-writeups/trans-advice-agent/",
+  },
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex],
